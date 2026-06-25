d94d655fedbfa9702e85a3aac41909ec17ac6aa3
Nusrahkhan <khannusrah0@gmail.com>
2026-06-25 15:55:39 +0530

    hopefully sign up fixed

--- frontend-html/api-client.js
+++ frontend-html/api-client.js
@@ -17,7 +17,7 @@ class APIClient {
     const response = await fetch(`${API_BASE_URL}/auth/signup`, {
       method: "POST",
       headers: { "Content-Type": "application/json" },
-      body: JSON.stringify({g
+      body: JSON.stringify({
         name,
         email,
         password,
